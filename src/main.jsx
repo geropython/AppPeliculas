f410a40939552cb0334a528105c30aca05c64d7a
geropython <72422102+geropython@users.noreply.github.com>
2025-03-10 16:54:15 -0300

    app peliculas subido

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { MovieApp } from './MovieApp'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MovieApp />
   </StrictMode>,
 )
